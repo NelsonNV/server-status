c8a5c068db4469e4b1d0fb4879068c1ca88eb47d
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-07 21:54:58 -0400

    feat: agregar datos json

--- server-status.py
+++ server-status.py
@@ -5,3 +5,14 @@ def leer_json(file="server.json"):
         data = json.load(lectura)
     return data
 
+
+def agregar_dato(file="server.json", service_name="", dns="", ip="", alt=""):
+    data = leer_json(file)
+
+    nuevo_dato = {service_name: {"dns": dns, "ip": ip, "alt": alt}}
+
+    data.update(nuevo_dato)
+
+    with open(file, "w") as escritura:
+        json.dump(data, escritura, indent=4)
+
